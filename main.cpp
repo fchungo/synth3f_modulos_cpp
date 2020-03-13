d90edac6196ace0db9bc28c75e32124584aafc7e
imazzala <ivanmazzalay@gmail.com>
2020-03-13 12:48:51 -0300

    Testeando

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Synth3f
+//
+//  Created by Ivan Mazzalay on 13/03/2020.
+//  Copyright © 2020 Ivan Mazzalay. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
