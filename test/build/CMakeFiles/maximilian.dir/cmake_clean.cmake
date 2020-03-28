5d31de21ea20c4d7f69c208c1ac80bfde17ff0ee
fabricio <fabricio.chungo@gmail.com>
2020-03-27 21:22:17 -0300

    armando tests de todos los tipos de onda

---
+++ test/build/CMakeFiles/maximilian.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maximilian.dir/main.cpp.o"
+  "CMakeFiles/maximilian.dir/player.cpp.o"
+  "CMakeFiles/maximilian.dir/RtAudio.cpp.o"
+  "CMakeFiles/maximilian.dir/home/fabri/git/synth3f_modulos_cpp/src/maximilian.cpp.o"
+  "maximilian.pdb"
+  "maximilian"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/maximilian.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- test/main.cpp
+++ test/main.cpp
@@ -1,15 +1,49 @@
 #include "maximilian.h"
+#include <iostream>
+#include <stdlib.h>
+
 
 //This shows how the fundamental building block of digital audio - the sine wave.
 //
 maxiOsc mySine;//One oscillator - can be called anything. Can be any of the available waveforms.
+
+float freq = 440;
+double signal;
+double duty = 0.5;
+bool input = false;
+int option;
+
+double selectMode(int mode){
+	double result;
+	switch (mode)
+	{
+		case 0:
+			result = mySine.sinewave(freq);
+			break;
+		case 1:
+			result = mySine.sawn(freq);
+			break;
+		case 2:
+			result = -mySine.sawn(freq);
+			break;
+		case 3:
+			result = mySine.triangle(freq);
+			break;
+		case 4:
+			result = mySine.pulse(freq, duty);
+			break;
+	}
+}
+
 void setup() {//some inits
-    //nothing to go here this time
+	srand (time(NULL));
+	std::cout << "Frecuencia: " << freq << std::endl;
+	option = rand() % 5;
+	std::cout << "Waveform selected: " << option << std::endl;
 }
 
 void play(double *output) {
-
-    output[0]=mySine.sinewave(440);
-    output[1]=output[0];
-    
+	signal = selectMode(option);
+    output[0]=signal;
+    output[1]=output[0];    
 }
